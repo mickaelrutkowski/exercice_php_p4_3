a6b2e6579a7d8e51db02b9294b629bef6f8f07b9
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-21 09:03:35 +0100

    exercice_php_P3_3

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+  $name = 'mickael';
+  $firstName = 'rutkowski';
+  function afficheName($name, $firstName)
+  {
+    return $name;
+    return $firstName;
+  }
+  echo $name . ' ' . $firstName;
+  ?>
+</body>
+</html>
